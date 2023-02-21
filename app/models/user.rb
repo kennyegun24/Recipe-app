b64b136cc981e1fd7f2fafc6fc7664c1b252e2b6
jaamanu <jaamanu@gmail.com>
2023-02-21 21:13:45 +0200

    Add user.rb file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :recipes, foreign_key: "user_id", class_name: "Recipe"
+    has_many :foods, foreign_key: "user_id", class_name: "Food"
+    
+    validates :name, presence: true, length: { maximum: 100 }
 end
