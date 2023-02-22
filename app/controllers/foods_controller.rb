f1899c075ac471e736e5613662423e9df320f764
jaamanu <kennyegun240@gmail.com>
2023-02-22 21:13:03 +0100

    Authentication with devise

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,4 +1,25 @@
 class FoodsController < ApplicationController
     def index
+        @foods = Food.all
+    end
+
+    def new
+        @food = Food.new
+    end
+    
+    def create
+      @food = Food.new(food_params)
+      @food.user = current_user
+      if @food.save
+        flash[:notice] = 'food successfully created!'
+        redirect_to root_path(current_user)
+      else
+        flash.now[:error] = 'Error: Error occurred when creating post'
+        render :new
+      end
+    end
+
+    def food_params
+        params.permit(:name, :measuerment_unit, :price, :quantity)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root "foods#index"
-  get "recipes", to: "recipes#index"
-  get 'recipes/:id', to: 'recipes#show'
-  get "public_recipes", to: 'public_recipes#index'
-  get 'general_shopping_lists', to: 'general_shopping#index'
+  # get "recipes", to: "recipes#index"
+  # get 'recipes/:id', to: 'recipes#show'
+  # get "public_recipes", to: 'public_recipes#index'
+  # get 'general_shopping_lists', to: 'general_shopping#index'
+
+  resources :foods, only: [:new, :create]
+  resources :recipes, only: [:index, :show, :new, :create]
+  resources :comments, only: [:new, :create]
+  resources :likes, only: [:create]
+  
 end
