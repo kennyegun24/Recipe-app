85a26f8dc606c2ad395c6807f91520f664f6449a
jaamanu <kennyegun240@gmail.com>
2023-02-24 21:16:41 +0100

    Modify food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -6,4 +6,5 @@ class Food < ApplicationRecord
     validates :measuerment_unit, presence: true
     validates :price, presence: true
     validates :quantity, presence: true
+    validates :quantity, :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
